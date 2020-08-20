5d8ee21f60242dafbe19b9d7bdbd5167336ac0b8
yul091 <47262464+yul091@users.noreply.github.com>
2020-08-19 23:05:25 -0500

    my-version

--- se_tasks/clone_detection/run_experiment.sh
+++ se_tasks/clone_detection/run_experiment.sh
@@ -9,7 +9,7 @@ fi
 
 
 EPOCHS=50
-BATCH=128
+BATCH=64
 LR=0.005
 TRAIN_DATA='se_tasks/clone_detection/data/train.pkl'
 TEST_DATA='se_tasks/clone_detection/data/test.pkl'
@@ -42,70 +42,69 @@ TEST_DATA='se_tasks/clone_detection/data/test.pkl'
 # --experiment_name=$EXPERIMENT_NAME #| tee $EXPERIMENT_LOG
 
 
-
-EMBEDDING_TYPE=0
-EMBEDDING_DIM=100
-EMBEDDING_PATH='embedding_vec/100_2/Doc2VecEmbedding0.vec'
-EXPERIMENT_NAME='doc2vec_cowb'
-EXPERIMENT_LOG=$RES_DIR$EXPERIMENT_NAME'.txt'
-echo $EXPERIMENT_NAME
-CUDA_VISIBLE_DEVICES=5 python -m se_tasks.clone_detection.scripts.main \
---train_data=$TRAIN_DATA --test_data=$TEST_DATA --embed_type=$EMBEDDING_TYPE \
---epochs=$EPOCHS --batch=$BATCH --lr=$LR \
---embed_dim=$EMBEDDING_DIM --embed_path=$EMBEDDING_PATH \
---experiment_name=$EXPERIMENT_NAME #| tee $EXPERIMENT_LOG
-
-
-EMBEDDING_TYPE=0
-EMBEDDING_DIM=100
-EMBEDDING_PATH='embedding_vec/100_2/Doc2VecEmbedding1.vec'
-EXPERIMENT_NAME='doc2vec_skipgram'
-EXPERIMENT_LOG=$RES_DIR$EXPERIMENT_NAME'.txt'
-echo $EXPERIMENT_NAME
-CUDA_VISIBLE_DEVICES=5 python -m se_tasks.clone_detection.scripts.main \
---train_data=$TRAIN_DATA --test_data=$TEST_DATA --embed_type=$EMBEDDING_TYPE \
---epochs=$EPOCHS --batch=$BATCH --lr=$LR \
---embed_dim=$EMBEDDING_DIM --embed_path=$EMBEDDING_PATH \
---experiment_name=$EXPERIMENT_NAME #| tee $EXPERIMENT_LOG
+# EMBEDDING_TYPE=0
+# EMBEDDING_DIM=100
+# EMBEDDING_PATH='embedding_vec/100_2/Doc2VecEmbedding0.vec'
+# EXPERIMENT_NAME='doc2vec_cowb'
+# EXPERIMENT_LOG=$RES_DIR$EXPERIMENT_NAME'.txt'
+# echo $EXPERIMENT_NAME
+# CUDA_VISIBLE_DEVICES=0 python -m se_tasks.clone_detection.scripts.main \
+# --train_data=$TRAIN_DATA --test_data=$TEST_DATA --embed_type=$EMBEDDING_TYPE \
+# --epochs=$EPOCHS --batch=$BATCH --lr=$LR \
+# --embed_dim=$EMBEDDING_DIM --embed_path=$EMBEDDING_PATH \
+# --experiment_name=$EXPERIMENT_NAME #| tee $EXPERIMENT_LOG
 
 
-EMBEDDING_TYPE=0
-EMBEDDING_DIM=100
-EMBEDDING_PATH='embedding_vec/100_2/Word2VecEmbedding0.vec'
-EXPERIMENT_NAME='word2vec_cowb'
-EXPERIMENT_LOG=$RES_DIR$EXPERIMENT_NAME'.txt'
-echo $EXPERIMENT_NAME
-CUDA_VISIBLE_DEVICES=5 python -m se_tasks.clone_detection.scripts.main \
---train_data=$TRAIN_DATA --test_data=$TEST_DATA --embed_type=$EMBEDDING_TYPE \
---epochs=$EPOCHS --batch=$BATCH --lr=$LR \
---embed_dim=$EMBEDDING_DIM --embed_path=$EMBEDDING_PATH \
---experiment_name=$EXPERIMENT_NAME #| tee $EXPERIMENT_LOG
+# EMBEDDING_TYPE=0
+# EMBEDDING_DIM=100
+# EMBEDDING_PATH='embedding_vec/100_2/Doc2VecEmbedding1.vec'
+# EXPERIMENT_NAME='doc2vec_skipgram'
+# EXPERIMENT_LOG=$RES_DIR$EXPERIMENT_NAME'.txt'
+# echo $EXPERIMENT_NAME
+# CUDA_VISIBLE_DEVICES=1 python -m se_tasks.clone_detection.scripts.main \
+# --train_data=$TRAIN_DATA --test_data=$TEST_DATA --embed_type=$EMBEDDING_TYPE \
+# --epochs=$EPOCHS --batch=$BATCH --lr=$LR \
+# --embed_dim=$EMBEDDING_DIM --embed_path=$EMBEDDING_PATH \
+# --experiment_name=$EXPERIMENT_NAME #| tee $EXPERIMENT_LOG
 
 
-EMBEDDING_TYPE=0
-EMBEDDING_DIM=100
-EMBEDDING_PATH='embedding_vec/100_2/Word2VecEmbedding1.vec'
-EXPERIMENT_NAME='word2vec_skipgram'
-EXPERIMENT_LOG=$RES_DIR$EXPERIMENT_NAME'.txt'
-echo $EXPERIMENT_NAME
-CUDA_VISIBLE_DEVICES=5 python -m se_tasks.clone_detection.scripts.main \
---train_data=$TRAIN_DATA --test_data=$TEST_DATA --embed_type=$EMBEDDING_TYPE \
---epochs=$EPOCHS --batch=$BATCH --lr=$LR \
---embed_dim=$EMBEDDING_DIM --embed_path=$EMBEDDING_PATH \
---experiment_name=$EXPERIMENT_NAME #| tee $EXPERIMENT_LOG
+# EMBEDDING_TYPE=0
+# EMBEDDING_DIM=100
+# EMBEDDING_PATH='embedding_vec/100_2/Word2VecEmbedding0.vec'
+# EXPERIMENT_NAME='word2vec_cowb'
+# EXPERIMENT_LOG=$RES_DIR$EXPERIMENT_NAME'.txt'
+# echo $EXPERIMENT_NAME
+# CUDA_VISIBLE_DEVICES=5 python -m se_tasks.clone_detection.scripts.main \
+# --train_data=$TRAIN_DATA --test_data=$TEST_DATA --embed_type=$EMBEDDING_TYPE \
+# --epochs=$EPOCHS --batch=$BATCH --lr=$LR \
+# --embed_dim=$EMBEDDING_DIM --embed_path=$EMBEDDING_PATH \
+# --experiment_name=$EXPERIMENT_NAME #| tee $EXPERIMENT_LOG
 
 
-EMBEDDING_TYPE=0
-EMBEDDING_DIM=100
-EMBEDDING_PATH='embedding_vec/100_2/FastEmbeddingcbow.vec'
-EXPERIMENT_NAME='fasttext_cowb'
-EXPERIMENT_LOG=$RES_DIR$EXPERIMENT_NAME'.txt'
-echo $EXPERIMENT_NAME
-CUDA_VISIBLE_DEVICES=5 python -m se_tasks.clone_detection.scripts.main \
---train_data=$TRAIN_DATA --test_data=$TEST_DATA --embed_type=$EMBEDDING_TYPE \
---epochs=$EPOCHS --batch=$BATCH --lr=$LR \
---embed_dim=$EMBEDDING_DIM --embed_path=$EMBEDDING_PATH \
---experiment_name=$EXPERIMENT_NAME #| tee $EXPERIMENT_LOG
+# EMBEDDING_TYPE=0
+# EMBEDDING_DIM=100
+# EMBEDDING_PATH='embedding_vec/100_2/Word2VecEmbedding1.vec'
+# EXPERIMENT_NAME='word2vec_skipgram'
+# EXPERIMENT_LOG=$RES_DIR$EXPERIMENT_NAME'.txt'
+# echo $EXPERIMENT_NAME
+# CUDA_VISIBLE_DEVICES=2 python -m se_tasks.clone_detection.scripts.main \
+# --train_data=$TRAIN_DATA --test_data=$TEST_DATA --embed_type=$EMBEDDING_TYPE \
+# --epochs=$EPOCHS --batch=$BATCH --lr=$LR \
+# --embed_dim=$EMBEDDING_DIM --embed_path=$EMBEDDING_PATH \
+# --experiment_name=$EXPERIMENT_NAME #| tee $EXPERIMENT_LOG
+
+
+# EMBEDDING_TYPE=0
+# EMBEDDING_DIM=100
+# EMBEDDING_PATH='embedding_vec/100_2/FastEmbeddingcbow.vec'
+# EXPERIMENT_NAME='fasttext_cowb'
+# EXPERIMENT_LOG=$RES_DIR$EXPERIMENT_NAME'.txt'
+# echo $EXPERIMENT_NAME
+# CUDA_VISIBLE_DEVICES=3 python -m se_tasks.clone_detection.scripts.main \
+# --train_data=$TRAIN_DATA --test_data=$TEST_DATA --embed_type=$EMBEDDING_TYPE \
+# --epochs=$EPOCHS --batch=$BATCH --lr=$LR \
+# --embed_dim=$EMBEDDING_DIM --embed_path=$EMBEDDING_PATH \
+# --experiment_name=$EXPERIMENT_NAME #| tee $EXPERIMENT_LOG
 
 
 EMBEDDING_TYPE=0
@@ -114,7 +113,7 @@ EMBEDDING_PATH='embedding_vec/100_2/FastEmbeddingskipgram.vec'
 EXPERIMENT_NAME='fasttext_skipgram'
 EXPERIMENT_LOG=$RES_DIR$EXPERIMENT_NAME'.txt'
 echo $EXPERIMENT_NAME
-CUDA_VISIBLE_DEVICES=5 python -m se_tasks.clone_detection.scripts.main \
+CUDA_VISIBLE_DEVICES=4 python -m se_tasks.clone_detection.scripts.main \
 --train_data=$TRAIN_DATA --test_data=$TEST_DATA --embed_type=$EMBEDDING_TYPE \
 --epochs=$EPOCHS --batch=$BATCH --lr=$LR \
 --embed_dim=$EMBEDDING_DIM --embed_path=$EMBEDDING_PATH \
@@ -122,40 +121,40 @@ CUDA_VISIBLE_DEVICES=5 python -m se_tasks.clone_detection.scripts.main \
 
 
 
-EMBEDDING_TYPE=0
-EMBEDDING_DIM=100
-EMBEDDING_PATH='embedding_vec/100_2/GloVeEmbeddingNone.vec'
-EXPERIMENT_NAME='glove'
-EXPERIMENT_LOG=$RES_DIR$EXPERIMENT_NAME'.txt'
-echo $EXPERIMENT_NAME
-CUDA_VISIBLE_DEVICES=5 python -m se_tasks.clone_detection.scripts.main \
---train_data=$TRAIN_DATA --test_data=$TEST_DATA --embed_type=$EMBEDDING_TYPE \
---epochs=$EPOCHS --batch=$BATCH --lr=$LR \
---embed_dim=$EMBEDDING_DIM --embed_path=$EMBEDDING_PATH \
---experiment_name=$EXPERIMENT_NAME #| tee $EXPERIMENT_LOG
+# EMBEDDING_TYPE=0
+# EMBEDDING_DIM=100
+# EMBEDDING_PATH='embedding_vec/100_2/GloVeEmbeddingNone.vec'
+# EXPERIMENT_NAME='glove'
+# EXPERIMENT_LOG=$RES_DIR$EXPERIMENT_NAME'.txt'
+# echo $EXPERIMENT_NAME
+# CUDA_VISIBLE_DEVICES=6 python -m se_tasks.clone_detection.scripts.main \
+# --train_data=$TRAIN_DATA --test_data=$TEST_DATA --embed_type=$EMBEDDING_TYPE \
+# --epochs=$EPOCHS --batch=$BATCH --lr=$LR \
+# --embed_dim=$EMBEDDING_DIM --embed_path=$EMBEDDING_PATH \
+# --experiment_name=$EXPERIMENT_NAME #| tee $EXPERIMENT_LOG
 
 
-EMBEDDING_TYPE=0
-EMBEDDING_DIM=100
-EMBEDDING_PATH='embedding_vec/100_2/ori_code2seq.vec'
-EXPERIMENT_NAME='code2seq'
-EXPERIMENT_LOG=$RES_DIR$EXPERIMENT_NAME'.txt'
-echo $EXPERIMENT_NAME
-CUDA_VISIBLE_DEVICES=5 python -m se_tasks.clone_detection.scripts.main \
---train_data=$TRAIN_DATA --test_data=$TEST_DATA --embed_type=$EMBEDDING_TYPE \
---epochs=$EPOCHS --batch=$BATCH --lr=$LR \
---embed_dim=$EMBEDDING_DIM --embed_path=$EMBEDDING_PATH \
---experiment_name=$EXPERIMENT_NAME #| tee $EXPERIMENT_LOG
+# EMBEDDING_TYPE=0
+# EMBEDDING_DIM=100
+# EMBEDDING_PATH='embedding_vec/100_2/ori_code2seq.vec'
+# EXPERIMENT_NAME='code2seq'
+# EXPERIMENT_LOG=$RES_DIR$EXPERIMENT_NAME'.txt'
+# echo $EXPERIMENT_NAME
+# CUDA_VISIBLE_DEVICES=7 python -m se_tasks.clone_detection.scripts.main \
+# --train_data=$TRAIN_DATA --test_data=$TEST_DATA --embed_type=$EMBEDDING_TYPE \
+# --epochs=$EPOCHS --batch=$BATCH --lr=$LR \
+# --embed_dim=$EMBEDDING_DIM --embed_path=$EMBEDDING_PATH \
+# --experiment_name=$EXPERIMENT_NAME #| tee $EXPERIMENT_LOG
 
 
-EMBEDDING_TYPE=0
-EMBEDDING_DIM=100
-EMBEDDING_PATH='embedding_vec/100_2/code2vec.vec'
-EXPERIMENT_NAME='code2vec'
-EXPERIMENT_LOG=$RES_DIR$EXPERIMENT_NAME'.txt'
-echo $EXPERIMENT_NAME
-CUDA_VISIBLE_DEVICES=5 python -m se_tasks.clone_detection.scripts.main \
---train_data=$TRAIN_DATA --test_data=$TEST_DATA --embed_type=$EMBEDDING_TYPE \
---epochs=$EPOCHS --batch=$BATCH --lr=$LR \
---embed_dim=$EMBEDDING_DIM --embed_path=$EMBEDDING_PATH \
---experiment_name=$EXPERIMENT_NAME #| tee $EXPERIMENT_LOG
+# EMBEDDING_TYPE=0
+# EMBEDDING_DIM=100
+# EMBEDDING_PATH='embedding_vec/100_2/code2vec.vec'
+# EXPERIMENT_NAME='code2vec'
+# EXPERIMENT_LOG=$RES_DIR$EXPERIMENT_NAME'.txt'
+# echo $EXPERIMENT_NAME
+# CUDA_VISIBLE_DEVICES=5 python -m se_tasks.clone_detection.scripts.main \
+# --train_data=$TRAIN_DATA --test_data=$TEST_DATA --embed_type=$EMBEDDING_TYPE \
+# --epochs=$EPOCHS --batch=$BATCH --lr=$LR \
+# --embed_dim=$EMBEDDING_DIM --embed_path=$EMBEDDING_PATH \
+# --experiment_name=$EXPERIMENT_NAME #| tee $EXPERIMENT_LOG
